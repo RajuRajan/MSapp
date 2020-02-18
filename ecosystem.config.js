c24a01b2698fb9f0af9a93444a52fe7142adbfa3
raja rajan <rajuart678@gmail.com>
2020-02-18 20:50:14 +0530

    key

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,6 +19,7 @@ module.exports = {
 
   deploy : {
     production : {
+     'pre-deploy':"git pull origin master && npm run-script build",
       key: '~/key/aws.pem',
       user : 'ubuntu',
       host : '3.6.90.52',
