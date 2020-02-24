728b8786fa00b37a996d17cedae56feb61ea2ac4
raja rajan <rajuart678@gmail.com>
2020-02-24 19:21:15 +0530

    --base

--- src/_helpers/api.js
+++ src/_helpers/api.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 
 export const API = Axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'http://3.6.90.52:5000',
     headers: {
         'Content-Type': 'application/json'
     }
